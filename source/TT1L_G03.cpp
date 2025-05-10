35ca4fcec620207e6ca03fa3290c6df7381c9ca2
Mutton9558 <69904687+Mutton9558@users.noreply.github.com>
2025-05-11 01:36:26 +0800

    Output formatting and vm struct

--- source/TT1L_G03.cpp
+++ source/TT1L_G03.cpp
@@ -3,65 +3,134 @@
 #include <sstream>
 #include <map>
 #include <string>
+#include <fstream>
 
-// Flags
-bool OF, UF, CF, ZF;
+struct VirtualMachine
+{
+    std::map<int, char> registers = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}};
+    std::map<std::string, bool> flags = {{"OF", 0}, {"UF", 0}, {"CF", 0}, {"ZF", 0}};
+    int PC = 0;
+    std::map<int, char> memoryAddresses;
+};
 
 // Memory locations
-std::map<int, std::string> memoryAddresses;
 std::stringstream memoryItem;
 
-std::string outputMemory()
+// Registers
+std::map<int, std::string> registers;
+
+void rotateLeft(int num, int rotateAmount)
 {
-    // declare ostringstream to write string data more efficiently
-    std::ostringstream memoryData;
-    for (int i = 0; i < 64; i++)
-    {
-        if (i % 8 == 0 && i != 0)
-        {
-            memoryData << ("\n");
-        }
-
-        memoryData << (memoryAddresses[i] + " ");
-    }
-
-    return memoryData.str();
+    num = (num << rotateAmount) | (num >> 8 - rotateAmount);
 }
 
-char checkByteRange(signed char x)
+void rotateRight(int num, int rotateAmount)
+{
+    num = (num >> rotateAmount) | (num << 8 - rotateAmount);
+}
+
+void checkByteRange(signed char x, VirtualMachine &vm)
 {
     if (x > 127)
     {
-        OF = 1;
+        vm.flags["OF"] = 1;
         x -= 127;
     }
     else if (x < -128)
     {
-        UF = 1;
+        vm.flags["UF"] = 1;
         x += 128;
     }
-
-    return x;
+    else if (x == 0)
+    {
+        vm.flags["ZF"] = 1;
+    }
 }
 
-void addToMemory(int memoryAddress, int val)
+void outputToFile(VirtualMachine vm)
 {
-    memoryItem.str("");
-    memoryItem << std::uppercase << std::setfill('0') << std::setw(2) << std::hex << (val & 0xFF);
-    memoryAddresses[memoryAddress] = memoryItem.str();
+    std::ostringstream registerText;
+    std::ostringstream memoryText;
+    std::ostringstream memoryItem;
+
+    registerText << "Registers: ";
+    for (int i = 0; i < 8; i++)
+    {
+        memoryItem.str("");
+        memoryItem.clear();
+        memoryItem << std::uppercase << std::setfill('0') << std::setw(2)
+                   << std::hex << (static_cast<int>(static_cast<unsigned char>(vm.registers[i])) & 0xFF);
+        registerText << memoryItem.str();
+
+        if (i == 7)
+        {
+            registerText << "#";
+        }
+        else
+        {
+            registerText << "  ";
+        }
+    }
+
+    std::ostringstream flagText;
+    flagText << "Flags    : ";
+    flagText
+        << vm.flags["OF"] << "   "
+        << vm.flags["UF"] << "   "
+        << vm.flags["CF"] << "   "
+        << vm.flags["ZF"] << "#";
+
+    std::ostringstream pcText;
+    pcText << "PC       : " << vm.PC;
+
+    memoryText << "Memory   : \n";
+    for (int i = 0; i < 64; i++)
+    {
+        memoryItem.str("");
+        memoryItem.clear();
+        memoryItem << std::uppercase << std::setfill('0') << std::setw(2)
+                   << std::hex << (static_cast<int>(static_cast<unsigned char>(vm.memoryAddresses[i])) & 0xFF);
+        memoryText << memoryItem.str() << "  ";
+
+        if ((i + 1) % 8 == 0)
+            memoryText << "\n";
+    }
+    memoryText << "#";
+
+    // temporary for testing purposes
+    std::cout << registerText.str() << std::endl;
+    std::cout << flagText.str() << std::endl;
+    std::cout << pcText.str() << std::endl;
+    std::cout << memoryText.str() << std::endl;
 }
 
 int main()
 {
+    // Initilize vm
+    VirtualMachine vm;
+
     // initialize memory
     for (int i = 0; i < 64; i++)
     {
-        addToMemory(i, 0);
+        // addToMemory(i, 0);
+        vm.memoryAddresses[i] = 0;
+    }
+    std::ifstream assmeblyProgram("test1.asm");
+    std::string instruction, command;
+    while (getline(assmeblyProgram, instruction))
+    {
+        command = "";
+        for(char ins : instruction){
+            if(ins != ' '){
+                command += ins;
+            } else {
+                break;
+            }
+        }
+        // if statements here
+        continue;
     }
 
-    // test
-    addToMemory(10, 14);
-    std::string memoryData = outputMemory();
-    std::cout << memoryData << std::endl;
+    outputToFile(vm);
     return 0;
 }
